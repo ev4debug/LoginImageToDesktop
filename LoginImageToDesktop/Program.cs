80dd803d12b65361e7b4ab4f8e969a2f2bda7700
Marco Alba <marco.alba@gmail.com>
2017-10-05 14:37:06 +0200

    First release

--- LoginImageToDesktop/Program.cs
+++ LoginImageToDesktop/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.ServiceProcess;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace LoginImageToDesktop
@@ -14,12 +15,14 @@ namespace LoginImageToDesktop
         /// </summary>
         static void Main()
         {
+#if (!DEBUG)
             ServiceBase[] ServicesToRun;
-            ServicesToRun = new ServiceBase[]
-            {
-                new Service1()
-            };
+            ServicesToRun = new ServiceBase[]{new Service1()};
             ServiceBase.Run(ServicesToRun);
+#else
+            Service1 service = new Service1();
+            service.Start();            
+#endif
         }
     }
 }
--- LoginImageToDesktop/Service1.cs
+++ LoginImageToDesktop/Service1.cs
@@ -1,17 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Diagnostics;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.ServiceProcess;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace LoginImageToDesktop
 {
     public partial class Service1 : ServiceBase
     {
+        private string sourcedir = ConfigurationManager.AppSettings.Get("sourcedir");
+        private string destinationdir = ConfigurationManager.AppSettings.Get("destinationdir");
+
+        private System.Timers.Timer timer;
+
         public Service1()
         {
             InitializeComponent();
@@ -19,10 +28,56 @@ namespace LoginImageToDesktop
 
         protected override void OnStart(string[] args)
         {
+            Start();
         }
 
         protected override void OnStop()
         {
+            timer.Stop();
+        }
+
+        public void Start()
+        {
+            CheckDirsDifference();
+            timer = new System.Timers.Timer(3600000); 
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(this.timer_Elapsed);
+            timer.Start();            
+        }
+
+        private void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            timer.Stop();
+            CheckDirsDifference();
+            timer.Start();
+        }
+
+        private void CheckDirsDifference()
+        {
+            var sourcefiles = Directory.GetFiles(sourcedir).ToList();
+            foreach (var file in sourcefiles)
+            {
+                MoveSourceToDestination(file);
+            }
+        }
+
+        private void MoveSourceToDestination(string file)
+        {
+            try
+            {
+                Bitmap img = new Bitmap(file);
+                var imageHeight = img.Height;
+                var imageWidth = img.Width;
+                img = null;
+                if (imageHeight == 1080 && imageWidth == 1920)
+                {
+                    if (!File.Exists(destinationdir + Path.GetFileName(file) + ".jpg"))
+                        File.Copy(file, destinationdir + Path.GetFileName(file) + ".jpg");
+                }
+            }
+            catch (Exception)
+            {
+
+            }
         }
     }
 }
